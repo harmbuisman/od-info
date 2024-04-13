ed8e3daf299c24d63f5bedd7208127f561ca8d0c
Harm Buisman <harmbuisman@gmail.com>
2024-04-13 13:20:20 +0200

    rearranged files

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+python -m pip install -e .["dev"]
+"""
+
+from setuptools import find_packages, setup
+
+test_packages = [
+    "pytest>=5.4.3",
+    "black==23.12.1",
+    "flake8==7.0.0",
+    "isort==5.12.0",
+]
+
+base_packages = [
+    "flask",
+    "jinja2",
+    "PyYAML",
+    "bs4"
+]
+
+setup(
+    name="od-info",
+    packages=find_packages(exclude=["notebooks", "docs", "vantage6"]),
+    install_requires=base_packages,
+    python_requires=">=3.10",
+)
